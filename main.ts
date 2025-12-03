987385114465a1f049bf7c1fa6e0f235021ad221
DimiS0 <24132721@student.hhs.nl>
2025-12-03 15:15:08 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,16 @@ input.onButtonPressed(Button.A, function () {
     bluetooth.uartWriteString("PHOTO/n")
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    // debug: zet tijdelijk aan als je wilt checken wat er binnenkomt
-    // basic.showString(data)
+
     data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
     if (data == "1") {
-        // plastic gevonden
-        // servo naar plastic‑positie
+
         servos.P0.setAngle(90)
         basic.showIcon(IconNames.Yes)
         basic.pause(500)
         basic.clearScreen()
     } else {
-        // geen plastic (of iets anders dan "1")
-        // servo terug
+
         servos.P0.setAngle(0)
         basic.showLeds(`
             # . . . #
